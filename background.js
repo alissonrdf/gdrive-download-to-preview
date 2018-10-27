7fade48a87b731cbea931369f41b7dfd258cc2a4
alissonrdf <32949545+alissonrdf@users.noreply.github.com>
2018-10-27 06:15:17 -0400

    Add files via upload

---
+++ background.js
@@ -0,0 +1,21 @@
+browser.contextMenus.create({
+    id: "gdrive-download-to-view",
+    title: "Visualizar link do Google Drive",
+    contexts: ["link"],
+    icons: {
+        "16": "icons/extension-16.png",
+        "32": "icons/extension-32.png"
+      }
+});
+browser.contextMenus.onClicked.addListener((info, tab) => {
+    if (info.menuItemId === "gdrive-download-to-view") {
+        // Obtendo o link de download.
+        const download_url = info.linkUrl;
+        
+        // Adicionando o começo do novo link + id retirado do link de download + final do novo link
+        const view_url = "https://drive.google.com/file/d/" + /id=([^&]+)/.exec(download_url)[1] + "/view";
+
+        // Criando uma nova aba não ativa, ao lado da aba atual
+        browser.tabs.create({url: view_url, active: false, index: tab.index+1})
+    }
+});
